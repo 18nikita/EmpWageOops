a1d1d792b4bdc0257b485cfb9b6858c95631ac55
unknown <rajputnikita5666@gmail.com>
2021-09-28 22:44:43 +0530

    Added:Emp wage for multiple company

--- src/com/bz/empwage/EmpWage.java
+++ src/com/bz/empwage/EmpWage.java
@@ -2,47 +2,40 @@ package com.bz.empwage;
 
 public class EmpWage {
 
+	private static final int IS_PART_TIME = 1;
+	private static final int IS_FULL_TIME = 2;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage");
-		EmployeeWage();
+		EmpWageForMultipleCompany("Dmart", 20, 2, 10 );
+		EmpWageForMultipleCompany("Relience", 10, 4, 20 );
 	}
-
-	// Constatnts
-	static final int IS_FULL_TIME = 1;
-	static final int IS_PART_TIME = 2;
-	static final int EMP_RATE_PER_HOUR = 20;
-	static final int WORKING_DAYS_IN_MONTH = 20;
-	static final int MAX_HOURS_IN_MONTH = 100;
-
-	public static void EmployeeWage() {
-
-		// Loacl Variables
-		int empHrs = 0;
-		int totalWorkday = 0;
-		int totalEmpHrs = 0;
-
-		while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkday < WORKING_DAYS_IN_MONTH) {
-			totalWorkday++;
-			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-			System.out.println("empCheck : " + empCheck);
-			System.out.println("WorkDay : " + totalWorkday);
-			switch (empCheck) {
-			case IS_FULL_TIME:
-				System.out.println("Employee is Full Time present");
-				empHrs = 8;
-				break;
+	
+	public static int EmpWageForMultipleCompany(String company, int empRatePerHour, int NoOfWorkDay, int MaxHourPerMonth ) {
+		
+		//variables
+		int empHour=0, totalEmpHour=0, totalWorkingDays=0;
+		//computation
+		while(totalEmpHour<=MaxHourPerMonth && totalWorkingDays<=NoOfWorkDay) {
+			totalWorkingDays++;
+			
+			int empCheck=(int) Math.random()*3;
+			switch(empCheck) {
 			case IS_PART_TIME:
-				System.out.println("Employee is Part Time Present");
-				empHrs = 4;
+				empHour=4;
 				break;
+			case IS_FULL_TIME:
+				empHour=8;
+				break;	
 			default:
-				System.out.println("Employee is Absent");
-				empHrs = 0;
+				empHour=0;
 			}
-			totalEmpHrs += empHrs;
-			System.out.println("totalWorkday" + " Emp Hrs :" + totalEmpHrs);
+			totalEmpHour += empHour;
+			System.out.println("Day"+totalWorkingDays+ "Emp Hour "+empHour);
 		}
-		int totalEmpWage = EMP_RATE_PER_HOUR * totalEmpHrs;
-		System.out.println("total Emp Wage: " + totalEmpWage);
+		int totalEmpWage=totalEmpHour*empRatePerHour;
+		System.out.println("Total Employee wage for company "+ company+" is "+totalEmpWage);
+		return totalEmpWage;
 	}
+
 }
