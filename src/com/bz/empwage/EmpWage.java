5280e5242dbce1d3c6b09f745a2d738dedc5efa1
unknown <rajputnikita5666@gmail.com>
2021-09-28 13:39:35 +0530

    Added:Wage till condition reach

--- src/com/bz/empwage/EmpWage.java
+++ src/com/bz/empwage/EmpWage.java
@@ -4,39 +4,46 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage");
-		WageForMonth();
+		WageTillCondition();
 	}
-	
-	public static void WageForMonth() {
-		// UC-5-Emp wage for month
-		
-		// Constants
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
-		int WORKING_DAYS_IN_MONTH = 20;
 
+	// Constatnts
+	static final int IS_FULL_TIME = 1;
+	static final int IS_PART_TIME = 2;
+	static final int EMP_RATE_PER_HOUR = 20;
+	static final int WORKING_DAYS_IN_MONTH = 20;
+	static final int MAX_HOURS_IN_MONTH = 100;
+
+	public static void WageTillCondition() {
+		// UC-6-Wage till condition of total working hour is reached in month
 		// Variables
 		int empHrs = 0;
-		int empWage = 0;
+		int totalWorkday = 0;
+		int totalEmpHrs = 0;
+		int totalWage = 0;
 
-		// Computation
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-
-		if (empCheck == IS_PART_TIME) {
-			empHrs = 4;
-			System.out.println("Employee is PartTime");
+		while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkday < WORKING_DAYS_IN_MONTH) {
+			totalWorkday++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("empCheck : " + empCheck);
+			System.out.println("WorkDay : " + totalWorkday);
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				System.out.println("Employee is Full Time present");
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				System.out.println("Employee is Part Time Present");
+				empHrs = 4;
+				break;
+			default:
+				System.out.println("Employee is Absent");
+				empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("totalWorkday" + " Emp Hrs :" + totalEmpHrs);
 		}
-
-		else if (empCheck == IS_FULL_TIME) {
-			empHrs = 8;
-			System.out.println("Employee is FullTime");
-		} else {
-			System.out.println("Employee is Absent");
-		}
-
-		// Formula
-		empWage = empHrs * EMP_RATE_PER_HOUR * WORKING_DAYS_IN_MONTH;
-		System.out.println("Employee Wage is: " + empWage);
+		int totalEmpWage = EMP_RATE_PER_HOUR * totalEmpHrs;
+		System.out.println("total Emp Wage: " + totalEmpWage);
 	}
 }
