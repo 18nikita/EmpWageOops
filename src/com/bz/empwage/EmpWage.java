637eaa8508c85a9e3d463a8368b49fb430ca3178
unknown <rajputnikita5666@gmail.com>
2021-09-28 13:34:53 +0530

    Added:Wage for Month

--- src/com/bz/empwage/EmpWage.java
+++ src/com/bz/empwage/EmpWage.java
@@ -4,43 +4,39 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage");
-		UsingSwitchCase();
+		WageForMonth();
 	}
-
-	public static void UsingSwitchCase() {
-		// UC-4-Solve employee wage using case statement
-
+	
+	public static void WageForMonth() {
+		// UC-5-Emp wage for month
+		
 		// Constants
-		final int IS_FULL_TIME = 1;
-		final int IS_PART_TIME = 2;
-		final int EMP_RATE_PER_HOUR = 20;
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+		int WORKING_DAYS_IN_MONTH = 20;
 
 		// Variables
 		int empHrs = 0;
 		int empWage = 0;
 
 		// Computation
-		int empCheck = (int) (Math.random() * 100) % 3;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 
-		switch (empCheck) {
-		case IS_FULL_TIME:
-			empHrs = 8;
-			System.out.println("Employee is present full time");
-			break;
-
-		case IS_PART_TIME:
+		if (empCheck == IS_PART_TIME) {
 			empHrs = 4;
-			System.out.println("Employee is present part time");
-			break;
-
-		default:
-			empHrs = 0;
-			System.out.println("Employee is absent");
-			break;
-
+			System.out.println("Employee is PartTime");
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Wage is :" + empWage);
 
+		else if (empCheck == IS_FULL_TIME) {
+			empHrs = 8;
+			System.out.println("Employee is FullTime");
+		} else {
+			System.out.println("Employee is Absent");
+		}
+
+		// Formula
+		empWage = empHrs * EMP_RATE_PER_HOUR * WORKING_DAYS_IN_MONTH;
+		System.out.println("Employee Wage is: " + empWage);
 	}
 }
