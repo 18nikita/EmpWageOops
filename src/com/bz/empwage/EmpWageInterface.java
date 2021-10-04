f8480422e75126782e235a4e104221ba9b193c93
unknown <rajputnikita5666@gmail.com>
2021-10-04 16:00:20 +0530

    Added:Emp wage interface

---
+++ src/com/bz/empwage/EmpWageInterface.java
@@ -0,0 +1,6 @@
+package com.bz.empwage;
+
+public class EmpWageInterface {
+	public void addCompanyWage(String companyName, int max_working_hrs, int days_in_month, int wage_per_hr);
+    public void computeEmpWage();
+}
